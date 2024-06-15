9c679a98c2d9974c9b1d5c3e80d2c8be84630e1f
Rizvi Fazi <rizvifazi124@gmail.com>
2024-06-15 23:59:51 +0530

    Before configuring lombok

---
+++ src/main/java/com/pack/SpringUserDetails/UserConfig.java
@@ -0,0 +1,26 @@
+package com.pack.SpringUserDetails;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Configuration
+@PropertySource("file:\\C:\\user_properties\\user.properties")
+@ConfigurationProperties(prefix="user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserConfig {
+	private String fname;
+	private String lname;
+	private Integer age;
+	private String gender;
+	private String city;	
+}
+
